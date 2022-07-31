8308087821bf42f038db7ad1ce4ba22c345dab4a
Stewart <stewart.kerns@gmail.com>
2022-07-30 17:56:51 -0700

    Finished main code of Lab3 and GuessGame, need to add comments

--- src/SKerns_lab3/GuessGame.java
+++ src/SKerns_lab3/GuessGame.java
@@ -1,2 +1,61 @@
-package SKerns_lab3;public class GuessGame {
+package SKerns_lab3;
+
+import java.util.Random;
+
+public class GuessGame {
+    private int rangeMinimum;
+    private int rangeMaximum;
+    private int count;
+    private int target;
+
+    public GuessGame(){
+        rangeMinimum = 1;
+        rangeMaximum = 100;
+        count = 0;
+        newTarget();
+    }
+
+    public void newTarget(){
+        Random rand = new Random();
+        target = rand.nextInt((rangeMaximum - rangeMinimum) + 1) +
+                rangeMinimum;
+    }
+
+    public boolean guess(int num){
+        if (num == target) {
+            return true;
+        }
+        else{
+            displayHint(num);
+            return false;
+        }
+    }
+    private void displayHint(int num){
+        if (num < target){
+            System.out.println(num + " is too low.");
+        }
+        else {
+            System.out.println(num + " is too high.");
+        }
+    }
+    public void incrementCount(){
+        count++;
+    }
+
+    public int getRangeMinimum(){
+        return rangeMinimum;
+    }
+
+    public int getRangeMaximum(){
+        return rangeMaximum;
+    }
+
+    public int getCount(){
+        return count;
+    }
+
+
+
+
+
 }
--- src/SKerns_lab3/Lab3.java
+++ src/SKerns_lab3/Lab3.java
@@ -1,2 +1,61 @@
-package SKerns_lab3;public class Lab3 {
+package SKerns_lab3;
+
+import java.util.Scanner;
+
+public class Lab3 {
+
+    public static void main(String[] args){
+    Scanner keyboardIn = new Scanner(System.in);
+    welcome();
+    playGame(keyboardIn);
+    goodbye();
+    keyboardIn.close();
+    }
+
+    public static void playGame(Scanner keyboardIn){
+        do{
+            boolean guessBool;
+            GuessGame game = new GuessGame();
+            System.out.println();
+            do {
+                guessBool = game.guess(guessNum(keyboardIn, game));
+//                if (guessBool) {
+//                    System.out.println("That's correct!\nYou guessed " +
+//                           game.getCount() + " times.");
+//                }
+            } while (!guessBool);
+            System.out.println("\nThat's correct!\nYou guessed " +
+                    game.getCount() + " times.");
+        } while(!noPlay(keyboardIn));
+
+    }
+
+    public static boolean noPlay(Scanner keyboardIn){
+        final String stringEnd = "NO";
+        System.out.print("\nWould you like to play again? (no to quit): ");
+        keyboardIn.nextLine();
+        String userAnswer = keyboardIn.nextLine();
+        return userAnswer.equalsIgnoreCase(stringEnd);
+    }
+
+    public static int guessNum(Scanner keyboardIn, GuessGame game){
+        int guess;
+        do {
+            game.incrementCount();
+            System.out.print("Guess a number between " + game.getRangeMinimum() +
+                    " and " + game.getRangeMaximum() + ": ");
+            guess = keyboardIn.nextInt();
+        } while(guess < game.getRangeMinimum() || guess >
+                game.getRangeMaximum());
+        return guess;
+    }
+
+    public static void welcome(){
+        System.out.println("This is a guessing game where you will guess a " +
+                "number\nand I will you if it is too low or too high.\n");
+    }
+
+    public static void goodbye(){
+        System.out.println("\nThanks for playing!");
+    }
 }
